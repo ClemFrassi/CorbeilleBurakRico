fdccdcbb5467807e37d2c8c0dd83e2bb808c763f
Clément <Clment@DESKTOP-M99F7LI>
2018-04-24 11:38:45 +0200

    Classes dimension et position

---
+++ Dimension.java
@@ -0,0 +1,30 @@
+
+public class Dimension implements Mobile {
+	private int width;
+	private int height;
+	
+	public Dimension(int width, int height) {
+	}
+	
+	public Dimension dimension(Dimension dimension) {
+	}
+	
+	//---------------------
+	public int getWidth() {
+		return width;
+	}
+	
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	//---------------------
+	public int getHeight() {
+		return height;
+	}
+	
+	public void setHeight(int height) {
+		this.height = height;
+	}
+	//---------------------
+}
+
